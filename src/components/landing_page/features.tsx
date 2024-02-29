b12658c983fb0dc89a23106f86dd831ff97b8f72
afesuazo <afesuazo@gmail.com>
2024-02-28 20:23:50 -0600

    Add feature interface and type hints

--- src/components/landing_page/features.tsx
+++ src/components/landing_page/features.tsx
@@ -1,33 +1,17 @@
 import {useEffect, useState} from "react";
 
-const features = [
-    {
-        key: 0,
-        title: "Feature 1",
-        description: "Description of feature 1"
-    },
-    {
-        key: 1,
-        title: "Feature 2",
-        description: "Description of feature 2"
-    },
-    {
-        key: 2,
-        title: "Feature 3",
-        description: "Description of feature 3"
-    },
-    {
-        key: 3,
-        title: "Feature 4",
-        description: "Description of feature 4"
-    }
-]
+const features: Feature[] = [
+    {key: 0, title: "Feature 1", description: "Description of feature 1"},
+    {key: 1, title: "Feature 2", description: "Description of feature 2"},
+    {key: 2, title: "Feature 3", description: "Description of feature 3"},
+    {key: 3, title: "Feature 4", description: "Description of feature 4"},
+];
 
 const Features = ({isLockHovered}: FeaturesProps) => {
 
     const [isEnabledColorActive, setIsEnabledColorActive] = useState(false);
-    const [titleTexts, setTitleTexts] = useState(features.map(feature => feature.title));
-    const [descriptionTexts, setDescriptionTexts] = useState(features.map(feature => feature.description));
+    const [titleTexts, setTitleTexts] = useState<string[]>(features.map(feature => feature.title));
+    const [descriptionTexts, setDescriptionTexts] = useState<string[]>(features.map(feature => feature.description));
 
     const scrambleText = (text: string) => {
         return text.split('').map((char) => {
@@ -63,15 +47,14 @@ const Features = ({isLockHovered}: FeaturesProps) => {
     }, [isLockHovered]);
 
     return (
+
         <div
             className={`drop-shadow-2xl relative grid px-6 mr-24 py-12 mb-12 rounded-r-full content-center bg-gray-200 w-full gap-2 lg:mb-0 lg:grid-cols-4 lg:text-left`}>
-
             <div
                 className={`absolute -z-5 h-full rounded-r-full transition-width duration-700 ease-in-out ${isLockHovered ? 'w-full' : 'w-0'} ${isEnabledColorActive ? 'bg-green-500' : 'bg-white'}`}/>
-
             {features.map((feature, index) => (
-                <div
-                    className="rounded-2xl z-10 h-max border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+                <div key={feature.key}
+                     className="rounded-2xl z-10 h-max border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
                     <h2 className="text-center mb-3 text-2xl font-semibold">
                         {titleTexts[index]}
                     </h2>
@@ -80,7 +63,6 @@ const Features = ({isLockHovered}: FeaturesProps) => {
                     </p>
                 </div>
             ))}
-
         </div>
 
     )
