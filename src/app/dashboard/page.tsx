8730a0a7f677bcb1bad290d4ec6d7f42750ac494
afesuazo <afesuazo@gmail.com>
2024-03-06 20:18:27 -0600

    Base dashboard layout division

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,58 +6,69 @@ export default function Dashboard() {
 
     const [selectedPassword, setSelectedPassword] = useState<boolean>(true)
 
-    const passwords= [
-         {
-              "id": 1,
-              "site": {
+    const passwords = [
+        {
+            "id": 1,
+            "site": {
                 "id": 1,
                 "name": "Google",
                 "url": "https://www.google.com",
                 "icon": "google_icon.png"
-              },
-              "username": "testuser",
-              "password": "testpassword",
-              "category": "Social",
-              "notes": "Test Notes"
-         },
-         {
-              "id": 2,
-              "site": {
+            },
+            "username": "testuser",
+            "password": "testpassword",
+            "category": "Social",
+            "notes": "Test Notes"
+        },
+        {
+            "id": 2,
+            "site": {
                 "id": 2,
                 "name": "Facebook",
                 "url": "https://www.facebook.com",
                 "icon": "facebook_icon.png"
-              },
-              "username": "testuser",
-              "password": "testpassword",
-              "category": "Social",
-              "notes": "Test Notes"
-         },
-         {
-              "id": 3,
-              "site": {
+            },
+            "username": "testuser",
+            "password": "testpassword",
+            "category": "Social",
+            "notes": "Test Notes"
+        },
+        {
+            "id": 3,
+            "site": {
                 "id": 3,
                 "name": "Twitter",
                 "url": "https://www.twitter.com",
                 "icon": "twitter_icon.png"
-              },
-              "username": "testuser",
-              "password": "testpassword",
-              "category": "Social",
-              "notes": "Test Notes"
-         }
-         ];
+            },
+            "username": "testuser",
+            "password": "testpassword",
+            "category": "Social",
+            "notes": "Test Notes"
+        }
+    ];
+
+    const toggleDetailsPanel = () => {
+        setSelectedPassword(!selectedPassword);
+    };
 
     return (
         <main className="flex flex-row h-screen justify-between p-2 ">
-            <div className="w-full h-full bg-emerald-400 rounded-2xl">
-            {/* Passwords Table and Filters */}
+            <div
+                className={`transition-width duration-500 ease-in-out ${selectedPassword ? 'w-2/3' : 'w-full'} h-full bg-emerald-400 rounded-2xl p-6`}>
+                {/* Passwords Table and Filters */}
+                <button
+                    className="p-2 rounded-2xl bg-blue-500"
+                    onClick={toggleDetailsPanel}>
+                    Toggle
+                </button>
             </div>
-            {selectedPassword &&
-                <div className="w-1/3 h-full border-l bg-emerald-200 rounded-2xl">
-                    {/*  Details for a selected credential */}
-                </div>
-            }
+            <div
+                className={`transition-all duration-200 ease-in-out ${selectedPassword ? 'flex-grow p-6' : 'w-0'} h-full border-l rounded-2xl bg-emerald-200`}>
+                {/* Details for a selected credential */}
+            </div>
+
+
         </main>
     );
 }
